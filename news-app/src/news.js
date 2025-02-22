921a8d1aa977ea4346f522a4a4c5c33768e39231
kkavinkk <gk.kavin05@gmail.com>
2025-02-21 21:14:37 -0700

    news.js complate

--- news-app/src/news.js
+++ news-app/src/news.js
@@ -1,5 +1,6 @@
 const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=7161a594098c49ecb0873c3b17e8d0c8'
 
-export default function getNews() {
-    let result
+export async function getNews() {
+    let result = await fetch(url).then(Response => Response.json)
+    return result.articles;
 }
