77e2cf2052e61901b5e975666e3414c4d5588eac
kkavinkk <gk.kavin05@gmail.com>
2025-02-22 22:10:39 -0700

    plzzzz

--- news-app/App.js
+++ news-app/App.js
@@ -4,6 +4,6 @@ import { FlatList }  from 'react-native'
 import { getNews } from './src/news.js';
 
 export default class App extends react.component {
-    constructor // FIND OUT WHAT TS DOES
+    constructor(props) // FIND OUT WHAT TS DOES
 }
 
