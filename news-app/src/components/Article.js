d528eb925fa30b5e168c3116bd10407b8291d137
kkavinkk <gk.kavin05@gmail.com>
2025-02-25 12:03:57 -0700

    working on articles default.png is vvery epic

--- news-app/src/components/Article.js
+++ news-app/src/components/Article.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { View, Linking, TouchableNativeFeedback } from 'react-native';
-import { Text, Button, Card, Divider } from 'react-native-elements'
+import { Text, Button, Card, Divider, makeStyles } from 'react-native-elements'
 import moment from "moment";
 
 export default class Article extends React.Component {
-    render()
+    render() {
+        const {
+            title,
+            description,
+            publishedAt,
+            source,
+            urlToImage,
+            url
+        } = this.props.article;
+        const { noteStyle, featuredTitleStyle } = styles;
+        const time = moment(publishedAt || moment.now()).fromNow();
+        const defaulting = 'default.png';
+
+        return (
+            <TouchableNativeFeedback
+            useForeground
+            onPress={() => Linking.openURL(url)}
+        )
+    }
 }
