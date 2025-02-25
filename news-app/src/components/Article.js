cdcc15f200e8479c3dcef5782a6d6dc450d05486
kkavinkk <gk.kavin05@gmail.com>
2025-02-25 11:53:32 -0700

    it feels like im doing sm wrong

---
+++ news-app/src/components/Article.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Linking, TouchableNativeFeedback } from 'react-native';
+import { Text, Button, Card, Divider } from 'react-native-elements'
+import moment from "moment";
+
+export default class Article extends React.Component {
+    render()
+}
